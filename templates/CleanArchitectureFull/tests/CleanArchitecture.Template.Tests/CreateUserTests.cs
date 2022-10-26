a78125ff4bc91693508f7d6410b25e2454480dbd
Sergey Ivanov <goodfoxorg@gmail.com>
2022-10-26 15:01:06 -0400

    Add 2 more unit tests for CreateUserHandler to showcase different capabilities of the chosen tools.

--- templates/CleanArchitectureFull/tests/CleanArchitecture.Template.Tests/CreateUserTests.cs
+++ templates/CleanArchitectureFull/tests/CleanArchitecture.Template.Tests/CreateUserTests.cs
@@ -1,5 +1,5 @@
 using CleanArchitecture.Template.Application.User.CreateUser;
-using FluentAssertions;
+using FluentValidation;
 using FluentValidation.Results;
 using OneOf;
 
@@ -12,11 +12,62 @@ public class CreateUserHandlerTests {
         var handler = new CreateUserHandler(validator);
         var request = new CreateUserCommand("John", "Doe");
 
-        var newUser = await handler.Handle(request, default);
+        var response = await handler.Handle(request, default);
+        var newUser = response.AsT0;
 
-        newUser.Should().NotBeNull();
-        newUser.Should().BeOfType<OneOf<CreateUserResponse, ValidationResult>>();
-        newUser.IsT0.Should().BeTrue();
-        newUser.AsT0.UserId.Should().NotBeEmpty();
+        response.Should().BeOfType<OneOf<CreateUserResponse, ValidationResult>>();
+        response.IsT0.Should().BeTrue();
+        newUser.UserId.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Validator_is_called_when_the_handler_is_invoked() {
+        var validatorMock = new Mock<IValidator<CreateUserCommand>>();
+        var handler = new CreateUserHandler(validatorMock.Object);
+        var request = new CreateUserCommand("John", "Doe");
+        validatorMock.Setup(x => x.Validate(request)).Returns(new ValidationResult());
+
+        _ = await handler.Handle(request, default);
+
+        validatorMock.Verify(x => x.Validate(request), Times.Once);
+    }
+
+    [Theory]
+    [InlineData("", "Doe")]
+    [InlineData("John", "")]
+    [InlineData("", "")]
+    [InlineData(null, "Doe")]
+    [InlineData("John", null)]
+    [InlineData(null, null)]
+    public async Task Given_invalid_input_returns_appropriate_errors(string firstName, string lastName) {
+        var validator = new CreateUserCommandValidator();
+        var handler = new CreateUserHandler(validator);
+        var request = new CreateUserCommand(firstName, lastName);
+        const string errorForFirstName = "'First Name' must not be empty.";
+        const string errorForLastName = "'Last Name' must not be empty.";
+
+        var response = await handler.Handle(request, default);
+        var errorResult = response.AsT1;
+
+        response.Should().BeOfType<OneOf<CreateUserResponse, ValidationResult>>();
+        response.IsT1.Should().BeTrue();
+
+        switch (firstName, lastName) {
+            case var tuple
+            when string.IsNullOrEmpty(tuple.firstName)
+              && string.IsNullOrEmpty(tuple.lastName):
+                errorResult.Errors.Should().HaveCount(2);
+                errorResult.Errors[0].ErrorMessage.Should().BeEquivalentTo(errorForFirstName);
+                errorResult.Errors[1].ErrorMessage.Should().BeEquivalentTo(errorForLastName);
+                break;
+            case var tuple when tuple.firstName?.Length > 0:
+                errorResult.Errors.Should().HaveCount(1);
+                errorResult.Errors[0].ErrorMessage.Should().BeEquivalentTo(errorForLastName);
+                break;
+            case var tuple when tuple.lastName?.Length > 0:
+                errorResult.Errors.Should().HaveCount(1);
+                errorResult.Errors[0].ErrorMessage.Should().BeEquivalentTo(errorForFirstName);
+                break;
+        };
     }
 }
